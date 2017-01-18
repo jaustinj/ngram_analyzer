ed95659b6e56ada3a8690251ff9a7648cd0369f2
Austin Jacobs <austinjacobs@gmail.com>
2017-01-17 16:35:15 -0800

    ADD: created make.sh file

---
+++ make.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#make sure pip is installed
+sudo easy_install pip
+
+#make sure virtualenv is installed
+sudo pip install virtualenv
+sudo pip install --upgrade virtualenv
+
+#Create virtual environment .env
+virtualenv .env
+source .env/bin/activate
+pip install -r requirements.txt
+
+#create executable
+echo -e "cd $PWD\nsource .env/bin/activate\npython ngrams.py" >> run.command
+
+#Authorize Executable
+chmod u+x run.command
--- ngrams.py
+++ ngrams.py
@@ -2,7 +2,6 @@ from collections import Counter
 import itertools
 import os
 import re
-import scipy as sp
 import sys
 import time
 from tkinter import *
@@ -11,6 +10,7 @@ import warnings
 
 import numpy as np
 import pandas as pd
+import scipy as sp
 import xlsxwriter
 
 def get_filename():
@@ -52,7 +52,7 @@ def initialize_stopwords_list(user_stopwords = []):
 
     english_stop_words = ["a","able","about","above","abst","accordance","according","accordingly","across","act","actually","added","adj","affected","affecting","affects","after","afterwards","again","against","ah","all","almost","alone","along","already","also","although","always","am","among","amongst","an","and","announce","another","any","anybody","anyhow","anymore","anyone","anything","anyway","anyways","anywhere","apparently","approximately","are","aren","arent","arise","around","as","aside","ask","asking","at","auth","available","away","awfully","b","back","be","became","because","become","becomes","becoming","been","before","beforehand","begin","beginning","beginnings","begins","behind","being","believe","below","beside","besides","between","beyond","biol","both","brief","briefly","but","by","c","ca","came","can","cannot","can't","cause","causes","certain","certainly","co","com","come","comes","contain","containing","contains","could","couldnt","d","date","did","didn't","different","do","does","doesn't","doing","done","don't","down","downwards","due","during","e","each","ed","edu","effect","eg","eight","eighty","either","else","elsewhere","end","ending","enough","especially","et","et-al","etc","even","ever","every","everybody","everyone","everything","everywhere","ex","except","f","far","few","ff","fifth","first","five","fix","followed","following","follows","for","former","formerly","forth","found","four","from","further","furthermore","g","gave","get","gets","getting","give","given","gives","giving","go","goes","gone","got","gotten","h","had","happens","hardly","has","hasn't","have","haven't","having","he","hed","hence","her","here","hereafter","hereby","herein","heres","hereupon","hers","herself","hes","hi","hid","him","himself","his","hither","home","how","howbeit","however","hundred","i","id","ie","if","i'll","im","immediate","immediately","importance","important","in","inc","indeed","index","information","instead","into","invention","inward","is","isn't","it","itd","it'll","its","itself","i've","j","just","k","keep","kept","keeps","kg","km","know","known","knows","l","largely","last","lately","later","latter","latterly","least","less","lest","let","lets","like","liked","likely","line","little","'ll","look","looking","looks","ltd","m","made","mainly","make","makes","many","may","maybe","me","mean","means","meantime","meanwhile","merely","mg","might","million","miss","ml","more","moreover","most","mostly","mr","mrs","much","mug","must","my","myself","n","na","name","namely","nay","nd","near","nearly","necessarily","necessary","need","needs","neither","never","nevertheless","new","next","nine","ninety","no","nobody","non","none","nonetheless","noone","nor","normally","nos","not","noted","nothing","now","nowhere","o","obtain","obtained","obviously","of","off","often","oh","ok","okay","old","omitted","on","once","one","ones","only","onto","or","ord","other","others","otherwise","ought","our","ours","ourselves","out","outside","over","overall","owing","own","p","page","pages","part","particular","particularly","past","per","perhaps","placed","please","plus","poorly","possible","possibly","potentially","pp","predominantly","present","previously","primarily","probably","promptly","proud","provides","put","q","que","quickly","quite","qv","r","ran","rather","rd","re","readily","really","recent","recently","ref","refs","regarding","regardless","regards","related","relatively","research","respectively","resulted","resulting","results","right","run","s","said","same","saw","say","saying","says","sec","section","see","seeing","seem","seemed","seeming","seems","seen","self","selves","sent","seven","several","shall","she","shed","she'll","shes","should","shouldn't","show","showed","shown","showns","shows","significant","significantly","similar","similarly","since","six","slightly","so","some","somebody","somehow","someone","somethan","something","sometime","sometimes","somewhat","somewhere","soon","sorry","specifically","specified","specify","specifying","still","stop","strongly","sub","substantially","successfully","such","sufficiently","suggest","sup","sure","take","t","taken","taking","tell","tends","th","than","thank","thanks","thanx","that","that'll","thats","that've","the","their","theirs","them","themselves","then","thence","there","thereafter","thereby","thered","therefore","therein","there'll","thereof","therere","theres","thereto","thereupon","there've","these","they","theyd","they'll","theyre","they've","think","this","those","thou","though","thoughh","thousand","throug","through","throughout","thru","thus","til","tip","to","together","too","took","toward","towards","tried","tries","truly","try","trying","ts","twice","two","u","un","under","unfortunately","unless","unlike","unlikely","until","unto","up","upon","ups","us","use","used","useful","usefully","usefulness","uses","using","usually","v","value","various","'ve","very","via","viz","vol","vols","vs","w","want","wants","was","wasnt","way","we","wed","welcome","we'll","went","were","werent","we've","what","whatever","what'll","whats","when","whence","whenever","where","whereafter","whereas","whereby","wherein","wheres","whereupon","wherever","whether","which","while","whim","whither","who","whod","whoever","whole","who'll","whom","whomever","whos","whose","why","widely","willing","wish","with","within","without","wont","words","world","would","wouldnt","www","x","y","yes","yet","you","youd","you'll","your","youre","yours","yourself","yourselves","you've","z","zero"]
     spanish_stop_words = ["él","ésta","éstas","éste","éstos","última","últimas","último","últimos","a","añadió","aún","actualmente","adelante","además","afirmó","agregó","ahí","ahora","al","algún","algo","alguna","algunas","alguno","algunos","alrededor","ambos","ante","anterior","antes","apenas","aproximadamente","aquí","así","aseguró","aunque","ayer","bajo","bien","buen","buena","buenas","bueno","buenos","cómo","cada","casi","cerca","cierto","cinco","comentó","como","con","conocer","consideró","considera","contra","cosas","creo","cual","cuales","cualquier","cuando","cuanto","cuatro","cuenta","da","dado","dan","dar","de","debe","deben","debido","decir","dejó","del","demás","dentro","desde","después","dice","dicen","dicho","dieron","diferente","diferentes","dijeron","dijo","dio","donde","dos","durante","e","ejemplo","el","ella","ellas","ello","ellos","embargo","en","encuentra","entonces","entre","era","eran","es","esa","esas","ese","eso","esos","está","están","esta","estaba","estaban","estamos","estar","estará","estas","este","esto","estos","estoy","estuvo","ex","existe","existen","explicó","expresó","fin","fue","fuera","fueron","gran","grandes","ha","había","habían","haber","habrá","hace","hacen","hacer","hacerlo","hacia","haciendo","han","hasta","hay","haya","he","hecho","hemos","hicieron","hizo","hoy","hubo","igual","incluso","indicó","informó","junto","la","lado","las","le","les","llegó","lleva","llevar","lo","los","luego","lugar","más","manera","manifestó","mayor","me","mediante","mejor","mencionó","menos","mi","mientras","misma","mismas","mismo","mismos","momento","mucha","muchas","mucho","muchos","muy","nada","nadie","ni","ningún","ninguna","ningunas","ninguno","ningunos","no","nos","nosotras","nosotros","nuestra","nuestras","nuestro","nuestros","nueva","nuevas","nuevo","nuevos","nunca","o","ocho","otra","otras","otro","otros","para","parece","parte","partir","pasada","pasado","pero","pesar","poca","pocas","poco","pocos","podemos","podrá","podrán","podría","podrían","poner","por","porque","posible","próximo","próximos","primer","primera","primero","primeros","principalmente","propia","propias","propio","propios","pudo","pueda","puede","pueden","pues","qué","que","quedó","queremos","quién","quien","quienes","quiere","realizó","realizado","realizar","respecto","sí","sólo","se","señaló","sea","sean","según","segunda","segundo","seis","ser","será","serán","sería","si","sido","siempre","siendo","siete","sigue","siguiente","sin","sino","sobre","sola","solamente","solas","solo","solos","son","su","sus","tal","también","tampoco","tan","tanto","tenía","tendrá","tendrán","tenemos","tener","tenga","tengo","tenido","tercera","tiene","tienen","toda","todas","todavía","todo","todos","total","tras","trata","través","tres","tuvo","un","una","unas","uno","unos","usted","va","vamos","van","varias","varios","veces","ver","vez","y","ya","yo"]
-    internet_stop_words = ['full', "http", "https", 'com', 'edu', 'www', 'follow','credit', 'tasty', 'bzfd', 'videos']
+    internet_stop_words = ['episode','followme','unilad', 'video', 'videos', 'comment', 'like', 'ladbible', 'theladbible', 'download', 'app', 'lmao', 'free', 'link', 'bio', 'tag', 'full', "http", "https", 'com', 'edu', 'www', 'follow','credit', 'tasty', 'bzfd', 'videos']
 
     stop_words = english_stop_words + spanish_stop_words + internet_stop_words + user_stopwords
     return stop_words
@@ -149,6 +149,21 @@ def tuple_to_series(df, row):
     return row
 
 def filter_title_words(df, row, title_column, stopwords, sentance_cutoff=None, filter_creator_names = True, creator_column = None):
+    '''Placed in df.iterrows loop - Takes title words from each row, uses regex to return only alphanum, then filters stopwords 
+    and creator names from results.  Also truncates list to sentance cutoff length if given
+
+    @params:
+    df                      -required  : The Pandas.DataFrame
+    row                     -required  : Row of Pandas.DataFrame from iterrows loop
+    title_column            -required  : The name of the Row Series containing title words
+    stopwords               -required  : A list of stopwords to filter out of results
+    sentance_cutoff         -optional  : Allows to filter list of words to a max limit (to speed up computation)
+    filter_creator_names    -optional  : Option to filter out creator names from results as found in creator column of row-level Series
+    creator_column          -optional  : The name of the Series containing creator names
+
+    Sample Input/Output:
+    'This is the coolest title in!the!WORLD!@*!&@*!DEFINITELY!)!'  => ['coolest', 'title', 'world']
+    '''
     if not creator_column:
         filter_creator_names = False
 
@@ -172,6 +187,8 @@ def filter_title_words(df, row, title_column, stopwords, sentance_cutoff=None, f
     return regex_words_list
 
 def get_words_in_creator_names(df, creator_column):
+    '''Filters out creator names from words --> see filter_title_words()
+    '''
     creators = list(set(df[creator_column].tolist()))
     words_in_creator_names = [str(creator).split(' ') for creator in creators]
     words_in_creator_names = [item for sublist in words_in_creator_names for item in sublist]
@@ -179,6 +196,15 @@ def get_words_in_creator_names(df, creator_column):
     return words_in_creator_names
 
 def get_ngrams(words_list, n=2):
+    '''Given a list of words, returns all combinations of ngrams without replacement, in alphabetical order
+
+    @params:
+    words_list     -required  :  a list of words
+    n              -required  :  number of words to choose for combination
+
+    Sample Input/Output:
+    ['Small', 'Word', 'List'] => [('List', 'Small'),('List','Word'),('Small','Word')]
+    '''
     ngram_iter = itertools.combinations(words_list,n)
     ngram_list = []
     for ngram in ngram_iter: #order bigrams so combination only has one order it will append to dict
@@ -190,6 +216,9 @@ def get_ngrams(words_list, n=2):
     return ngram_list
 
 def add_ngram_to_dict(row, ngram, ngram_dict, column_key):
+    '''Adds Column value to dictionary with key = current_ngram
+    
+    '''
     try:
         ngram_dict[column_key][ngram].append(row[column_key])
     except KeyError:
@@ -197,6 +226,10 @@ def add_ngram_to_dict(row, ngram, ngram_dict, column_key):
         ngram_dict[column_key][ngram].append(row[column_key])
 
 def sort_by_count(df):
+    '''returns Df, sorted by Word Count descending
+
+
+    '''
     df.head()
     df['word_count'] = df['index'].apply(lambda x: len(x))
     df = df.sort_values(by='word_count', ascending=False)
@@ -276,7 +309,9 @@ if __name__ == '__main__':
 
 
     print('\n(Part 3/6): Computing metric means...')
+    #take the means of aggregated values for each indicated column
     df = df_means(ngram_df, *columns_to_do_stats_on)
+    #take the medians of aggregated values for each indicated column
     print('\n(Part 4/6): Computing metric medians...')
     df = df_medians(df, *columns_to_do_stats_on)
     
@@ -287,6 +322,7 @@ if __name__ == '__main__':
         df[column] = df[column].apply(pandaCounter)
     df = df.join(pd.DataFrame(df['Platform'].to_dict()).T)
     df = df[df['word_count'] > 1]
+    df = df.reset_index()
 
     print('\n(Part 6/6): Saving file to Desktop...')
     save_file(outfile)
